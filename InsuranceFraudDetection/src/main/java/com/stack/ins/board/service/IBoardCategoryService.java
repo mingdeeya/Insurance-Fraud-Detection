b0e69e5844f5ac638a09843215415cb95792f149
gwooop <rjsdn7106@naver.com>
2020-04-01 13:53:42 +0900

    - views board.jsp 변경 - mvc-config.xml 컼트롤러 컴포넌트 추가 - application-config.xml 마이바티스, 서비스 추가

--- InsuranceFraudDetection/src/main/java/com/stack/ins/board/service/IBoardCategoryService.java
+++ InsuranceFraudDetection/src/main/java/com/stack/ins/board/service/IBoardCategoryService.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import com.stack.ins.board.model.BoardCategory;
 
-
 public interface IBoardCategoryService {
 	List<BoardCategory> selectAllCategory();
-	List<BoardCategory> selectAllCategoryByClass1(int class1);
 	void insertNewCategory(BoardCategory boardCategory);
 	void updateCategory(BoardCategory boardCategory);
 	void deleteCategory(int categoryId);
